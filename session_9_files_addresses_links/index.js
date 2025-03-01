d03d70888334c89f51cbcb14edee65ad47744076
Eman <relelrify@gmail.com>
2025-03-01 23:43:45 +0200

    session 12

---
+++ session_9_files_addresses_links/index.js
@@ -0,0 +1,8 @@
+console.log('Text content')
+console.log(document.getElementById('h').textContent)
+console.log('inner html')
+
+console.log(document.getElementById('h').innerHTML)
+console.log('inner text')
+
+console.log(document.getElementById('h').innerText)
